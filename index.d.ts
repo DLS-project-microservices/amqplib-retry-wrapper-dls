f279d7c08704a5e424c588bac168799007396f0d
masp1234 <mart2c04@stud.kea.dk>
2024-05-15 10:49:22 +0200

    added type definition file

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+import { Channel } from 'amqplib';
+
+interface ConfigOptions {
+    maxRetries?: number;
+    delaySeconds?: number;
+}
+
+/**
+ * Configures the retry options for connecting to RabbitMQ.
+ * @param options - An object containing the configuration options.
+ */
+export function config(options?: ConfigOptions): void;
+
+/**
+ * Connects to RabbitMQ with retry logic.
+ * @param url - The URL of the RabbitMQ server.
+ * @returns A promise that resolves to a RabbitMQ channel.
+ */
+export function connectToRabbitMQ(url: string): Promise<Channel>;
